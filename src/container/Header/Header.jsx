f92f48f6f759d07ebf8c9379c28e521ec255101f
Fabien Chareun <cacrfabfr@gmail.com>
2022-02-01 10:04:28 +0100

    header setup

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import { SubHeading } from '../../components';
 
+import { images } from '../../constants';
 import './Header.scss';
 
 const Header = () => (
-  <div>
-    Header
+  <div className="app__header app__wrapper section__padding" id="home">
+    <div className="app__wrapper_info">
+      <SubHeading title="Chase the new Flavour" />
+      <h1 className="app_header-h1">The key to Fine dining</h1>
+    </div>
+    <div className="app__wrapper_img">
+      
+    </div>
   </div>
 );
 
