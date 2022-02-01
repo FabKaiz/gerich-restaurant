d16508f5e7fb5d44ec274c0d92b2acb40719bf55
Fabien Chareun <cacrfabfr@gmail.com>
2022-02-01 10:20:55 +0100

    changed custom button colors and finished header

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -9,9 +9,15 @@ const Header = () => (
     <div className="app__wrapper_info">
       <SubHeading title="Chase the new Flavour" />
       <h1 className="app_header-h1">The key to Fine dining</h1>
+      <p className="p__opensans" style={{ margin: '2rem 0' }}>
+        Sit tellus lobortis sed senectus vivamus molestie.
+        Condimentum volutpat morbi facilisis quam scelerisque sapien.
+        Et, penatibus aliquam amet tellus 
+      </p>
+      <button type='button' className="custom__button">Explore Menu</button>
     </div>
     <div className="app__wrapper_img">
-      
+      <img src={images.welcome} alt="header" />
     </div>
   </div>
 );
