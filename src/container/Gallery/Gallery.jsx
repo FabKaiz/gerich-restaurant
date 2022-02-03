0d0a056becba2fb428e0f8b27e7668d887832f5f
Fabien Chareun <cacrfabfr@gmail.com>
2022-02-03 11:06:17 +0100

    galery setup

--- src/container/Gallery/Gallery.jsx
+++ src/container/Gallery/Gallery.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import { BsInstagram, BsArrowLeftShort, BsArrowRightShort } from 'react-icons/bs'
 
+import { SubHeading } from '../../components';
+import { images } from '../../constants';
 import './Gallery.scss';
 
-const Gallery = () => (
-  <div>
-    Gallery
-  </div>
-);
+const Gallery = () => {
+  return (
+    <div className="app__galery flex__center">
+      <div className="app__galery-content">
+        <SubHeading title='Instagram' />
+      </div>
+    </div>
+  )
+} ;
 
 export default Gallery;
